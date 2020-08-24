b047237a80310334d30e1fdb17a51a3748867411
Herb Bowie <hbowie@mac.com>
2020-08-24 13:05:39 -0700

    Initial Commit

---
+++ NotenikSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotenikSwiftUI
+//
+//  Created by Herb Bowie on 8/24/20.
+//  Copyright © 2020 PowerSurge Publishing. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
